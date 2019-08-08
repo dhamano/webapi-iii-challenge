72e998e1c3291ccf8bbb10615367d0929fa53f5c
Dustin Hamano <dhamano@Dustins-iMac-Pro.local>
2019-08-08 12:24:19 -0700

    added dynamic env port and start script for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require("./server.js");
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
